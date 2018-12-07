e9422321a3c51fc741eaf289338692befa295ce6
adrien cassar <adrien.cassar@etu.u-bordeaux.fr>
2018-12-07 05:28:49 +0100

    Affichage dynamique (enffiiinn ! en vrai c'était pas si difficile :/ )  + petit CSS plutôt sympa, vous m'en direz des nouvelles :D !

--- src/pages/alimentation/alimentation.ts
+++ src/pages/alimentation/alimentation.ts
@@ -18,57 +18,55 @@ export class AlimentationPage {
   //Nous on aime quand il y a plein de varriables ! pourquoi faire simple quand on peut faire compliquer sérieusement ?
   private hoursEat;
   private minutesEat;
-  private secondsEat;
   private preventEat;
+  private colorEat;
 
   private hoursDrink;
   private minutesDrink;
-  private secondsDrink;
   private preventDrink;
+  private colorDrink;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    //Un petit "date" avec l'équipe ? ;)
-    var lastEat = new Date(2018, 11, 6, 20, 0, 0, 0);
-    var lastDrink = new Date(2018, 11, 6, 20, 0, 0, 0);
 
+    this.colorEat = "secondary"; //et non uber-eats attention 
+    this.colorDrink = "secondary";
+    this.refresh();
+    this.timer();
+
+  }
+
+  refresh() {
+    var lastEat = new Date(2018, 11, 6, 20, 0, 0, 0);
+    var lastDrink = new Date(2018, 11, 7, 4, 30, 0, 0);
+
+    //Un petit "date" avec l'équipe ? ;)
     var now = new Date();
 
     this.hoursEat = Math.trunc(now.getTime() / 1000 / 60 / 60 - lastEat.getTime() / 1000 / 60 / 60);
     this.minutesEat = Math.trunc(now.getTime() / 1000 / 60 - lastEat.getTime() / 1000 / 60 - this.hoursEat * 60);
-    this.secondsEat = Math.trunc(now.getTime() / 1000 - lastEat.getTime() / 1000 - this.hoursEat * 3600 - this.minutesEat * 60);
 
     this.hoursDrink = Math.trunc(now.getTime() / 1000 / 60 / 60 - lastDrink.getTime() / 1000 / 60 / 60);
     this.minutesDrink = Math.trunc(now.getTime() / 1000 / 60 - lastDrink.getTime() / 1000 / 60 - this.hoursDrink * 60);
-    this.secondsDrink = Math.trunc(now.getTime() / 1000 - lastDrink.getTime() / 1000 - this.hoursDrink * 3600 - this.minutesDrink * 60);
+  
 
-    this.timer();
-
-    this.preventEat = "";
-    this.preventDrink = "";
+    //On s'est dit que toutes les 3 heures, c'était bien pour manger un petit do-mac entre deux foullées
+    if (this.hoursEat > 3) {
+      this.preventEat = "Il serait peut-être temps de manger un en-cas pour rester en forme !"; //on fait de la prévention ;)
+      this.colorEat = "danger";
+    }
+    //Si on part du principe que le climat est le même partout (ce qui n'est pas le cas bien évidemment), il faudrait s'hydrater toutes les 30 minutes (voir 15 si c'est possible et si on aime beaucoup l'eau !)
+    if (this.minutesDrink >= 30 || this.hoursDrink > 0) {
+      this.preventDrink = "Il serait peut-être temps de boire un peu d'eau pour rester en forme !"; //on fait de la prévention ;)
+      this.colorDrink = "danger";
+    }
 
   }
 
+  // on actualise le temps toutes les minutes, faudrait quand meme pas afficher les secondes non plus !
   timer() {
-    setInterval(function () {
-      //Un petit "date" avec l'équipe ? ;)
-      var lastEat = new Date(2018, 11, 6, 20, 0, 0, 0);
-    var lastDrink = new Date(2018, 11, 6, 20, 0, 0, 0);
-
-      var now = new Date();
-      console.log(this.secondsEat);
-
-      this.hoursEat = Math.trunc(now.getTime() / 1000 / 60 / 60 - lastEat.getTime() / 1000 / 60 / 60);
-      this.minutesEat = Math.trunc(now.getTime() / 1000 / 60 - lastEat.getTime() / 1000 / 60 - this.hoursEat * 60);
-      this.secondsEat = Math.trunc(now.getTime() / 1000 - lastEat.getTime() / 1000 - this.hoursEat * 3600 - this.minutesEat * 60);
-
-      this.hoursDrink = Math.trunc(now.getTime() / 1000 / 60 / 60 - lastDrink.getTime() / 1000 / 60 / 60);
-      this.minutesDrink = Math.trunc(now.getTime() / 1000 / 60 - lastDrink.getTime() / 1000 / 60 - this.hoursDrink * 60);
-      this.secondsDrink = Math.trunc(now.getTime() / 1000 - lastDrink.getTime() / 1000 - this.hoursDrink * 3600 - this.minutesDrink * 60);
-
-      //On s'est dit que toutes les 3 heures, c'était bien pour manger un petit do-mac entre deux foullées
-      if (this.hoursEat > 3) {
-        this.preventEat = "Il serait peut-être temps de manger un en-cas pour rester en forme !";
-      }
+    setInterval(() => {
+      this.refresh()
     }, 60000);
   }
 
