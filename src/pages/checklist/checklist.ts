64b0ad6454198a966dea1d990ba5c9b1ab2f524a
ndeguillaume <nicolasdeguillaume@gmail.com>
2018-12-07 04:40:30 +0100

    everything bf i gave up

--- src/pages/checklist/checklist.ts
+++ src/pages/checklist/checklist.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ElementRef } from '@angular/core';
 
 /**
  * Generated class for the ChecklistPage page.
@@ -8,18 +9,55 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+ 
 @IonicPage()
 @Component({
   selector: 'page-checklist',
   templateUrl: 'checklist.html',
 })
 export class ChecklistPage {
+todos: string[] = [];
+    todo: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    materials: string[] = [];
+    material: string;
+    
+
+
+    constructor(public navCtrl: NavController) {
+
+    }
+
+    addTodo() {
+      if (this.todo!="" && this.todo!=null){
+        this.todos.push(this.todo);
+        this.todo = "";
+      } 
+    }
+
+    addMaterial() {
+      if (this.material!="" && this.material!=null){
+        this.materials.push(this.material);
+        this.material = "";
+      }
+    }
+
+    delete(item,items) {
+        var index = items.indexOf(item, 0);
+        if (index > -1) {
+            items.splice(index, 1);
+        }
+    }
+
+    check(slidingItem) {
   }
+
+
+  
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ChecklistPage');
   }
+  
 
 }
